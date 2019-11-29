08382c1e8b1cfd5b0d41b1a5f9b84a51d186c543
WiZarD67810 <thomas.durst67@gmail.com>
2019-11-29 09:33:10 +0100

    Update

--- Eole_V2/src/Principale/FenAccueil.java
+++ Eole_V2/src/Principale/FenAccueil.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.JPanel;
 
-/*
+/**
  * Description : accueil
  * @author : Alice CHEREL Laura SCHAEFER
  * @version 1.0
@@ -13,27 +13,39 @@ import javax.swing.JPanel;
 
 public class FenAccueil extends JFrame{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Description : Initialisation composant JFrame
+	 * @author Laura SCHAEFERT & Alice CHEREL
+	 */
 	public FenAccueil() {
-		this.setTitle("Régate Eole");
+		this.setTitle("Rï¿½gate Eole");
 		getContentPane().setLayout(null);
 		//dimensions de la fenetre
 		this.setBounds(0,0, 900, 480);
 		//fenetre au centre
 		this.setLocationRelativeTo(null);
 		//non redimensionable
-		//this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
 		JPanel Fen = new JPanel();
 		Fen.setPreferredSize(new Dimension(100,20));
 		
 		//Bouton Nouvelle regate
-		JButton btnNouvelleRgate = new JButton("Nouvelle Régate");
+		JButton btnNouvelleRgate = new JButton("Nouvelle Rï¿½gate");
 		btnNouvelleRgate.setBounds(347, 334, 165, 60);
 		getContentPane().add(btnNouvelleRgate);
 		btnNouvelleRgate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				FenInscription fI = new FenInscription();
+				fI.setVisible(true);
+				btnNouvelleRgate.setEnabled(false);
+				btnNouvelleRgate.setText("Regate en cours...");
 			}
 		});
 		
